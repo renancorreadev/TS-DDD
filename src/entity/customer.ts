c3feba6eba12ff545c1437d728417e7d0ade865b
Renan Correa <developer@skynance.net>
2023-06-08 12:45:54 -0300

    feat: add object values in address

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -16,20 +16,24 @@ infra - Mundo externo
 - customer-ts (get, set)
 */
 
+import Address from "./address";
+
 export class Customer {
   _id: string;
   _name: string;
-  _address: string;
+  _address!: Address;
   _active: boolean = false;
 
   constructor(id: string, name: string, address: string) {
     this._id = id;
     this._name = name;
-    this._address = address;
     this.validate();
   }
 
   validate() {
+    if(this._address === undefined) {
+      throw new Error('Address is required');
+    }
     if(this._name.length === 0) {
       throw new Error('Name is required');
     }
@@ -42,16 +46,8 @@ export class Customer {
     this.validate();
   }
 
-  activate() {
-    if(this._address.length === 0) {
-      throw new Error('Address is required');
-    }else {
-      this._active = true;
-    }
-  }
-
-  deactivate() {
-    this._active = false;
+  changeAddress(address: Address) {
+    this._address = address;
   }
   
 }
